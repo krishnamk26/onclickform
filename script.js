ec459db9ec66c476f319f08749289ebe18c117ef
krishnamk26 <127760812+krishnamk26@users.noreply.github.com>
2023-04-16 14:09:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function foo(){
+    let res=document.getElementById("FirstName").value;
+    console.log(res);
+    let res1=document.getElementById("MiddleName").value;
+    console.log(res1);
+    let res2=document.getElementById("LastName").value;
+    console.log(res2);
+    let res3=document.getElementById("Email").value;
+    console.log(res3);
+    let res4=document.getElementById("Addres").value;
+    console.log(res4);
+}
